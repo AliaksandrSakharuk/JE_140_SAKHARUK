8b4f6e829856b58cc06ee097d1d6a9274ccf62fe
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-10-07 15:38:24 +0300

    start

---
+++ data_base-web-app/src/main/java/by/ita/je/Je140SakharukApplication.java
@@ -0,0 +1,12 @@
+package by.ita.je;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Je140SakharukApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Je140SakharukApplication.class, args);
+	}
+
+}
---
+++ data_base-web-app/src/test/java/by/ita/je/Je140SakharukApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ita.je;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Je140SakharukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
